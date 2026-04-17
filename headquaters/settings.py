7e13763a180359c0d2b783b8030caedfc16c7912
Saurabh Maurya <saurabhmauryajnp28@gmail.com>
2026-04-17 22:32:49 +0530

    Update settings.py

--- headquaters/settings.py
+++ headquaters/settings.py
@@ -119,11 +119,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+import os
 
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
