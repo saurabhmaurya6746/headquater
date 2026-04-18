dbfd02d2314e5c9e5c789558af7b64791cb3da8a
Saurabh Maurya <saurabhmauryajnp28@gmail.com>
2026-04-18 11:45:43 +0530

    Update settings.py

--- headquaters/settings.py
+++ headquaters/settings.py
@@ -30,7 +30,6 @@ DEBUG = True
 ALLOWED_HOSTS = ['headquater.onrender.com','headquater-main.onrender.com']
 
 from django.contrib.auth import get_user_model
-
 if os.environ.get("CREATE_SUPERUSER") == "True":
     User = get_user_model()
     if not User.objects.filter(username="admin").exists():
